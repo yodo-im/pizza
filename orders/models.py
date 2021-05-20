5bb99081178f394a52c0ec11d1f876353522ed00
yodo-im <78910536+yodo-im@users.noreply.github.com>
2021-05-20 17:20:17 +0300

    Fixed order database

--- orders/models.py
+++ orders/models.py
@@ -3,10 +3,10 @@ from mainapp.models import Product
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
+    first_name = models.CharField('Имя', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    email = models.EmailField('Почта',)
+    address = models.CharField('Адресс',max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
